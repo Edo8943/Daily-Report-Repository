0dc99ada00eaf1a59ecb539f28253d30a777a01f
Edo8943 <thegalaxyexpress111@icloud.com>
2021-04-14 22:08:32 +0900

    Add Empoyee show

--- src/controllers/employees/EmployeesShowServlet.java
+++ src/controllers/employees/EmployeesShowServlet.java
@@ -1,11 +1,17 @@
 package controllers.employees;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.persistence.EntityManager;
+
+import utils.DBUtil;
+import models.Employee;
 
 /**
  * Servlet implementation class EmployeesShowServlet
@@ -25,7 +31,20 @@ public class EmployeesShowServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        EntityManager em = DBUtil.createEntityManager();
+
+
+        Employee e = em.find(Employee.class, Integer.parseInt(request.getParameter("id")));
+
+
+        em.close();
+
+
+        request.setAttribute("employee", e);
+
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/show.jsp");
+        rd.forward(request,response);
     }
 
 }
